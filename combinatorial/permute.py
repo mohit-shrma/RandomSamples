287bccdc264349ccd22ebad506ac7756d7ee9e91
mohit sharma <mohit.786.sharma@gmail.com>
2012-04-12 20:07:07 -0500

    commented code for permutations

--- combinatorial/permute.py
+++ combinatorial/permute.py
@@ -3,11 +3,15 @@ import sys
 
 def permutations(str):
     allPerms = []
+    #return single character if passed
     if len(str) == 1:
         return str
+    #get all possible perms of string one char less
     allPrevPerms = permutations(str[0:-1])
+    #append last char in all possible loc in previous permutations 
     for prevPerm in allPrevPerms:
         for i in range(len(prevPerm)):
+            #append to the list the possible permutation generated
             allPerms.append(prevPerm[0:i] + str[-1] + prevPerm[i:])
         allPerms.append(prevPerm + str[-1])
     return allPerms
