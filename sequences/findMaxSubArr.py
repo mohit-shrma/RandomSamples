a6a114c52af0e63ea5ae2421edef96e2a5f33bc5
mohit sharma <mohit.786.sharma@gmail.com>
2012-08-09 07:32:10 +0530

    added max sub array computation using divide and conquer

---
+++ sequences/findMaxSubArr.py
@@ -0,0 +1,77 @@
+import sys
+import math
+
+""" find maximum subarray of passed contiguous array using divide & conquer,
+ nlog(n) complexity, reference cormen"""
+
+
+""" find maximum subarray of passed subarray arr,
+ and of range in between lo & hi indices"""
+def findMaxSubArr(arr, lo, hi):
+    if lo == hi:
+        #only one element (base case)
+        return (lo, hi, arr[lo])
+    else:
+        mid = int(math.floor((lo + hi) / 2))
+
+        #find max sub array in left of mid element
+        (leftLow, leftHigh, leftSum) = findMaxSubArr(arr, lo, mid)
+        
+        #find max sub array in right of mid element
+        (rightLow, rightHigh, rightSum) = findMaxSubArr(arr, mid + 1, hi)
+
+        #find max subarray which crosses mid element
+        (crossLow, crossHigh, crossSum) = findMaxCrossSubArr(arr, lo, mid, hi)
+
+        #return the largest of above computed three subarrays sums
+        if leftSum >= rightSum and leftSum >= crossSum:
+            return (leftLow, leftHigh, leftSum)
+        elif rightSum >= leftSum and rightSum >= crossSum:
+            return (rightLow, rightHigh, rightSum)
+        else:
+            return (crossLow, crossHigh, crossSum)
+        
+        
+
+""" find maximum subarray which crosses mid element of the array,
+ lo mid hi are indices"""
+
+def findMaxCrossSubArr(arr, lo, mid, hi):
+
+    #initialize it to -ve infinity or minimum int value possible
+    leftSum = -sys.maxint - 1
+    rightSum = -sys.maxint - 1
+    maxLeft = -1
+    maxRight = -1
+
+    tempSum = 0
+
+    for i in range(mid, lo-1, -1):
+        tempSum += arr[i]
+        if leftSum < tempSum:
+            leftSum = tempSum
+            maxLeft = i
+
+    tempSum = 0
+
+    for i in range(mid+1, hi+1):
+        tempSum += arr[i]
+        if rightSum < tempSum:
+            rightSum = tempSum
+            maxRight = i
+
+    return (maxLeft, maxRight, leftSum+rightSum)
+
+
+def main():
+    arr = [-10, 2, 4, -9, 6, 7 , -2, -8]
+    lo = 0
+    hi = len(arr) - 1
+    subArrLo, subArrHi, subArrSum = findMaxSubArr(arr, lo, hi)
+    print 'array: ', arr
+    print 'low, high, arrSum: ', subArrLo, subArrHi, subArrSum
+    print 'subArr: ', arr[subArrLo:subArrHi+1]
+
+ 
+if __name__ == '__main__':
+    main()
